a282995789f3bf2438251235b620f2f4998d95c5
nzemperl <nancyzempe@gmail.com>
2025-01-31 18:46:07 +0200

    update to accept banner choice

--- input.go
+++ input.go
@@ -3,19 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
-// Get input from command-line arguments
-func GetInput() string {
+// GetInput retrieves input string and chosen font
+func GetInput() (string, string) {
 	if len(os.Args) < 2 {
 		fmt.Println("Error: No input provided")
 		os.Exit(1)
 	}
-	return os.Args[1]
-}
 
-// Normalize input (convert to uppercase or lowercase)
-func NormalizeInput(input string) string {
-	return strings.ToUpper(input) // Adjust this depending on banner file requirements
+	input := os.Args[1] // First argument: the text to be converted
+	font := "standard"  // Default font
+
+	if len(os.Args) == 3 {
+		font = os.Args[2] // Second argument (if provided): the font type
+	}
+
+	return input, font
 }
