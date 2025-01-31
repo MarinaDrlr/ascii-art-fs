d3a2e4eed90533e071c32772275cf4a0395e38d8
nzemperl <nancyzempe@gmail.com>
2025-01-31 19:35:32 +0200

    update code to split the new line

--- process.go
+++ process.go
@@ -1,18 +1,29 @@
 package main
 
+import "strings"
+
 // GenerateASCIIArt constructs ASCII representation using the loaded banner
 func GenerateASCIIArt(input string, banner map[rune][]string) []string {
-	lines := make([]string, 8) // ASCII characters are 8 lines tall
+	lines := []string{}                      // To store the resulting ASCII art
+	inputLines := strings.Split(input, "\n") // Split input into separate lines
 
-	for _, char := range input {
-		asciiArt, exists := banner[char]
-		if !exists {
-			continue // Skip unknown characters
+	for _, line := range inputLines {
+		asciiLines := make([]string, 8) // ASCII characters are 8 lines tall
+
+		for _, char := range line {
+			asciiArt, exists := banner[char]
+			if !exists {
+				continue // Skip unknown characters
+			}
+
+			for i, artLine := range asciiArt {
+				asciiLines[i] += artLine + " "
+			}
 		}
 
-		for i, line := range asciiArt {
-			lines[i] += line + " "
-		}
+		// Add the generated ASCII art for the line to the result
+		lines = append(lines, asciiLines...)
+		lines = append(lines, "") // Add a blank line between each text line
 	}
 
 	return lines
