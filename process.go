4140a5c147ef1f34c85baf2f2d009121548503d3
nzemperl <nancyzempe@gmail.com>
2025-02-05 14:05:11 +0200

    added debugging

--- process.go
+++ process.go
@@ -1,6 +1,14 @@
 package main
 
+import "fmt"
+
 func GenerateASCIIArt(input string, banner map[rune][]string) []string {
+	// Debugging to verify inputs
+	fmt.Printf("Debug: Input to GenerateASCIIArt: %s\n", input)
+	fmt.Printf("Debug: Banner Map Size: %d\n", len(banner))
+	if len(banner) == 0 {
+		fmt.Println("Debug: Banner map is empty!")
+	}
 	lines := make([]string, 8) // Preallocate 8 lines for the ASCII output
 
 	for _, char := range input {
